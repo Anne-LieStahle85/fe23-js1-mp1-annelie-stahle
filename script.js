48d0a4def0315a64e6448757a4c7f2bfd081c7c9
Anne-LieStahle85 <149506235+Anne-LieStahle85@users.noreply.github.com>
2023-11-12 18:12:19 +0100

    lagt till kommentarer och tagit bort kommenterade rader med kod

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Här har jag gjort en array som jag sedan vill ska loopa med antal element i listan,
+//använt const hue för att färgen ska ändras per loop,samma med fontsize så den blir större för varje loop.
 const rader = ["Rad 1", "Rad 2", "Rad 3", "Rad 4", "Rad 5"];
 
 console.log(rader);
@@ -16,33 +18,22 @@ for (let i = 0; i < rader.length; i++) {
   h2.style.color = `hsl(210, 58%, 55% )`;
   h2.style.textAlign = "center";
   h2.style.fontSize = 10 + 10 * i + "px";
-  //h2.style.fontSize = "12px";
 
-  //const h2El = document.querySelectorAll("h2");
-  //let fontSize = 12;
-
-  //for (let i = 0; i<h2El.length; i++){
-  //  h2El[i].style.fontSize = fontSize + "px";
-  //fontSize += 5;
-
-  //}
 }
-//Här slutar rubrikerna
-//Här börjar listorna
 
-// if(i%2 == 0){
-//   h2.style.fontSize = "12px"
-//}
-//här är en container i en div som dom 3 listorna ska ligga i
+//Här har jag gjort en container med en div som dom 3 listorna ska ligga i och använt flex.
 const container = document.createElement("div");
 container.style.display = "flex";
-container.style.justifyContent = "space-evenly"
+container.style.justifyContent = "space-evenly";
 document.body.appendChild(container);
 container.style.border = "solid black";
 
 
-//Här börjar block 1 med första listan
-
+/*Här börjar blocken med listor där jag först använt en for-loop som skapar 3 st div:ar med samma utseende.
+Sen har jag gjort en till for-loop som skapar p:elementen i listorna 0-9,9-0,bokstäver.
+sedan har jag använt if och else if för att koda vilka p:element som ska ha en annan bakgrundsfärg/textfärg
+dom två första listorna har jag gjort på samma sätt medan jag valde att gör en array till sista listan där det
+skulle vara bokstäver istället för siffror*/ 
 for (let i = 0; i < 3; i++) {
   const block = document.createElement("div");
   container.appendChild(block);
@@ -58,7 +49,7 @@ for (let i = 0; i < 3; i++) {
 
     if (i == 1) {
       block.prepend(p);
-      p.style.textAlign = "center"
+      p.style.textAlign = "center";
     } else {
       block.appendChild(p);
     }
@@ -78,16 +69,13 @@ for (let i = 0; i < 3; i++) {
 
     if(i == 2 ){
         const block3 = ["Ett", "Två", "Tre", "Fyra", "Fem", "Sex", "Sju", "Åtta", "Nio", "Tio"];
+        console.log(block3);
+        
         p.textContent = block3[j];
-        p.style.textAlign = "right"
+        p.style.textAlign = "right";
         if(block3[j] == "Sex"){
             p.style.backgroundColor = "hsl(210, 58%, 55% )";
         }
     }
-    
-
-    
-
-  
   }
 }
